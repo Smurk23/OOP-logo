810735109f8d16266bb2a5e5431699ed59897267
Smurk23 <xingcantres@gmail.com>
2023-09-30 13:40:47 -0400

    added shapes to js

--- index.js
+++ index.js
@@ -2,26 +2,44 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-.prompt ([
-    {
-        type: 'input',
-        name: 'Text',
-        message: 'Enter text for your logo (up to three letters)',
-      },
-      {
-        type: 'input',
-        name: 'Text Color',
-        message: 'What color should your text be ?',
-      },
-      {
-        type: 'list',
-        name: 'Shape',
-        message: 'What shape do you want your logo to be ?',
-        choices: ['Circle', 'Square', 'Triangle'] 
-      },
-      {
-        type: 'input',
-        name: 'Shape Color',
-        message: 'What color do you want your shape to be ?',
-      }
+    .prompt([
+        {
+            type: 'input',
+            name: 'Text',
+            message: 'Enter text for your logo (up to three letters)',
+        },
+        {
+            type: 'input',
+            name: 'TextColor',
+            message: 'What color should your text be ?',
+        },
+        {
+            type: 'list',
+            name: 'Shape',
+            message: 'What shape do you want your logo to be ?',
+            choices: ['Circle', 'Square', 'Triangle']
+        },
+        {
+            type: 'input',
+            name: 'ShapeColor',
+            message: 'What color do you want your shape to be ?',
+        }
     ])
+
+    .then((answers) => {
+
+        let shape;
+        switch (answers.Shape) {
+            case 'Square':
+                shape = new Square();
+                break;
+            case 'Triangle':
+                shape = new Triangle();
+                break;
+            default:
+                shape = new Circle();
+                break;
+        }
+
+        shape.setColor (answers.ShapeColor);
+    })
